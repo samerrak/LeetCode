a71ff747ac0c3b1647567d64a1ff28aac364b347
Samer Abdulkarim <102871426+samerrak@users.noreply.github.com>
2022-07-31 07:19:20 -0400

    Update Roman2Int.java

--- Roman2Int.java
+++ Roman2Int.java
@@ -2,6 +2,17 @@ import java.util.*;
 
 
 public class Roman2Int {
+    //Alternatively
+    //            if (i + 1 < s.length()) {
+//                if (romanTable.get((s.charAt(i))) < romanTable.get((s.charAt(i+1)))){
+//                    val += (romanTable.get((s.charAt(i))) - romanTable.get((s.charAt(i+1)))) * -1;
+//                    i++;  }
+//                else
+//                    val += romanTable.get(s.charAt(i));
+//            }
+//            else
+//                val += romanTable.get(s.charAt(i));
+    
     public int romanToInt(String s) {
         HashMap<Character, Integer> romanTable = new HashMap<>(Map.of('I', 1, 'V', 5, 'X', 10, 'L', 50, 'C', 100, 'D', 500, 'M',1000));
         int val = 0;
