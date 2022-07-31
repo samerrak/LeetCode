accfc3a5c0cbb3740f06ad53a34b2bf07a624815
Samer Abdulkarim <102871426+samerrak@users.noreply.github.com>
2022-07-31 07:26:02 -0400

    Update Roman2Int.java

--- Roman2Int.java
+++ Roman2Int.java
@@ -2,44 +2,24 @@ import java.util.*;
 
 
 public class Roman2Int {
-    //Alternatively
-    //            if (i + 1 < s.length()) {
-//                if (romanTable.get((s.charAt(i))) < romanTable.get((s.charAt(i+1)))){
-//                    val += (romanTable.get((s.charAt(i))) - romanTable.get((s.charAt(i+1)))) * -1;
-//                    i++;  }
-//                else
-//                    val += romanTable.get(s.charAt(i));
-//            }
-//            else
-//                val += romanTable.get(s.charAt(i));
-    
     public int romanToInt(String s) {
-        HashMap<Character, Integer> romanTable = new HashMap<>(Map.of('I', 1, 'V', 5, 'X', 10, 'L', 50, 'C', 100, 'D', 500, 'M',1000));
+        HashMap<Character, Integer> romanTable = new HashMap<>();
+        romanTable.put('I', 1); romanTable.put('V', 5); romanTable.put('X', 10);
+        romanTable.put('L', 50); romanTable.put('C', 100); romanTable.put('D', 500); romanTable.put('M',1000);
         int val = 0;
         for (int i = 0; i < s.length(); i++){
-            try {
+            if (i + 1 < s.length()) {
                 if (romanTable.get((s.charAt(i))) < romanTable.get((s.charAt(i+1)))){
                     val += (romanTable.get((s.charAt(i))) - romanTable.get((s.charAt(i+1)))) * -1;
-                    i++;
-                }
+                    i++;  }
                 else
-                    val += romanTable.get(s.charAt(i));
-
-
-            }
-            catch (StringIndexOutOfBoundsException e){
-                val += romanTable.get(s.charAt(i));
-            }
-
-        }
+                    val += romanTable.get(s.charAt(i)); }
+            else
+                val += romanTable.get(s.charAt(i)); }
         return val;
     }
     public static void main (String[] args) {
-        // insert
         Roman2Int z = new Roman2Int();
-        // test 1: insert in an emptying tree
-        TST<Integer> tree = new TST<>();
-        tree.insert(1);
         Scanner scanner = new Scanner(System.in);
         int val = z.romanToInt(scanner.nextLine());
         System.out.println(val);
